d61cda2efaeb91277d387e0c7e7665b3de49283e
hank6086 <hank6086@gmail.com>
2020-01-18 16:05:39 +0800

    day1

--- Trymqtt_pub.py
+++ Trymqtt_pub.py
@@ -1,5 +1,5 @@
 import os
-import path.mqtt.client as mqtt
+import paho.mqtt.client as mqtt
 client = mqtt.Client()
 client.username_pw_set(os.getenv('MQTT_NAME'),os.getenv('MQTT_PW'))
 client.connect("192.168.1.101",1883,60)
