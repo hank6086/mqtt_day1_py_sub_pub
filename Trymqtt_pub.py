5eb6271bc170d1e50c7026be14168424251e155c
hank6086 <hank6086@gmail.com>
2020-01-18 17:03:11 +0800

    day1

--- Trymqtt_pub.py
+++ Trymqtt_pub.py
@@ -1,9 +1,12 @@
 import os
 import paho.mqtt.client as mqtt
+from dotenv import load_dotenv
+load_dotenv()
 client = mqtt.Client()
 client.username_pw_set(os.getenv('MQTT_NAME'),os.getenv('MQTT_PW'))
 client.connect("192.168.1.101",1883,60)
 
 test = "Hellol"
 print(test)
+print(os.getenv('MQTT_NAME') + " " + os.getenv('MQTT_PW')
 client.publish("Try/mqtt",test)
