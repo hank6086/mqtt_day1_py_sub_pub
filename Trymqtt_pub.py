6ca611794de41d3a01733a8069c482186294fd24
hank6086 <hank6086@gmail.com>
2020-01-19 15:09:33 +0800

    mysql

--- Trymqtt_pub.py
+++ Trymqtt_pub.py
@@ -1,12 +1,17 @@
 import os
 import paho.mqtt.client as mqtt
 from dotenv import load_dotenv
+import json
 load_dotenv()
 client = mqtt.Client()
 client.username_pw_set(os.getenv('MQTT_NAME'),os.getenv('MQTT_PW'))
-client.connect("192.168.1.101",1883,60)
+client.connect("127.0.0.1",1883,60)
 
-test = "Hellol"
-print(test)
-print(os.getenv('MQTT_NAME') + " " + os.getenv('MQTT_PW')
-client.publish("Try/mqtt",test)
+study = {
+    "name":"狗蛋香腸",
+    "number":"20",
+    "phone_number":"0912345678",
+}
+study = json.dumps(study,ensure_ascii=False)
+print(study)
+client.publish("Try/mqtt",study,)
